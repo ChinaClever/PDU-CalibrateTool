ac18ccb99b59008c087cc031ef55ad88b25f146a
luozhiyong <luozhiyong131@qq.com>
2020-06-18 11:35:48 +0800

    换成正

--- PDU-CalibrateTool/adjust/ad_autoid.cpp
+++ PDU-CalibrateTool/adjust/ad_autoid.cpp
@@ -50,7 +50,7 @@ QString Ad_AutoID::devTypeToStr(sDevType *dt)
     str += ch + tr("系列 ");
 
     str += tr("相数 %1").arg(dt->lines);
-    str += tr("输出位\回路 %1").arg(dt->outputs);
+    str += tr("输出位/回路 %1").arg(dt->outputs);
 
     if(1 == dt->specs) {
         str += tr("互感器 ");
